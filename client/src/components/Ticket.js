2dcbe44d3ca2190b266fb00301b98cd983b19af3
Daniel <dpericich@gmail.com>
2022-10-29 18:27:38 -0500

    added base styling and filler information

--- client/src/components/Ticket.js
+++ client/src/components/Ticket.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const TicketContainer = styled.div`
+  background-color: white;
+  color: black;
+  display: flex;
+  flex-direction: column;
+  justify-content: center
+  align-items: center;
+  padding: 1rem;
+  margin: 1rem;
+  width: 80%;
+  border-radius: 5px;
+  border: 1px dotted;
+  box-shadow: 3px 3px 2px rgba(255,255,255,0.7);
+`
 
 const Ticket = () => {
+  // Show the currently authenticated user -> only if a ticket has been minted
   return(
-    <div>This will be the ticket</div>
+    <TicketContainer>
+      <h3>Stacks Rails</h3>
+      <p>August 31st, 2023 - 6:00 PM</p>
+      <p>Rider: {"Rider's Name"}</p>
+      <p>Price: 35 STX</p>
+    </TicketContainer>
   )
 };
 
